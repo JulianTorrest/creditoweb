7f5b701b62cc232e091f6ef57334552fe4e12f2a
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-09-14 08:53:05 -0500

    Update dashboard_credito_express.py

--- dashboard_credito_express.py
+++ dashboard_credito_express.py
@@ -39,6 +39,7 @@ def generar_datos_ies():
         'Institución Pública o Privada': np.random.choice(['Pública', 'Privada'], n),
         'Renovaciones Realizadas': np.random.randint(1, 100, size=n),
         'Renovaciones Requeridas': np.random.randint(1, 100, size=n)
+        'Total Renovaciones Desembolsadas': np.random.randint(1, 100, size=n)
     })
     return data_ies
 
