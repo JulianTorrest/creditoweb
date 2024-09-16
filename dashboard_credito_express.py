f970f93e70cbc6abcfcccdea0442e1c768016788
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-09-15 19:53:20 -0500

    Update dashboard_credito_express.py

--- dashboard_credito_express.py
+++ dashboard_credito_express.py
@@ -2,6 +2,8 @@ import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Función para generar datos dummy para postulantes
 def generar_datos_dummy():
