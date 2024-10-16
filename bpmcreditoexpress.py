1a5ad193c745606529fb8c7bafd05ce554310bff
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-10-16 10:58:21 -0500

    Update bpmcreditoexpress.py

--- bpmcreditoexpress.py
+++ bpmcreditoexpress.py
@@ -408,8 +408,12 @@ def captura_datos():
     periodo = st.selectbox("Selecciona el periodo", options=["1er Semestre", "2do Semestre"])
     # Nuevo filtro para seleccionar la periodicidad
     periodicidad = st.selectbox("Selecciona la periodicidad", options=["Anual", "Semestral"])
+    st.subheader("Filtrar por Fecha")
+    fecha_inicio = st.date_input("Fecha de Inicio", value=datetime.today())
+    fecha_fin = st.date_input("Fecha de Fin", value=datetime.today())
 
     # Formulario actual de captura de datos
+    # Campos visibles del formulario
     st.subheader("Datos del Postulante")
     id_solicitud = st.text_input("ID Solicitud")
     nombre = st.text_input("Nombre")
@@ -418,6 +422,21 @@ def captura_datos():
     numero_documento = st.text_input("Número de Documento")
     fecha_solicitud = st.date_input("Fecha de Solicitud", value=datetime.today())
     estado_solicitud = st.selectbox("Estado de Solicitud", ["Pendiente", "Aprobada", "Rechazada"])
+
+    # Opción para mostrar/ocultar campos adicionales
+    if st.checkbox("Mostrar campos adicionales"):
+        # Campos ocultos
+        nacionalidad = st.multiselect("Nacionalidad", ["Colombiano", "Otro"])
+        edad = st.slider("Edad", min_value=18, max_value=65, value=(18, 65), step=1)
+        estado_credito = st.multiselect("Estado del crédito anterior (en caso de tener alguno)", ["Ninguno", "Castigado", "En mora y castigado"])
+        lista_sarlaft = st.multiselect("Lista SARLAFT", ["No está en ninguna lista", "Vinculantes", "Restrictivas", "Informativas"])
+        score_credito = st.slider("Score crediticio", min_value=150, max_value=900, value=(150, 900), step=1)
+        capacidad_pago = st.slider("Capacidad de pago (en COP)", min_value=1500000, max_value=20000000, value=(1500000, 20000000), step=10000)
+        limite_endeudamiento = st.slider("Límite de endeudamiento (en COP)", min_value=1500000, max_value=20000000, value=(1500000, 20000000), step=10000)
+        deudor = st.text_input("Nombre del deudor")
+        fecha_antecedentes = st.date_input("Fecha de antecedentes crediticios", value=datetime.today())
+        fecha_aplicación = st.date_input("Fecha de aplicación", value=datetime.today())
+
 
     if st.button("Mostrar datos de beneficiarios"):
         df_beneficiarios = pd.DataFrame(beneficiarios_data)
