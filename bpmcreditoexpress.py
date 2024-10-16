5ed6ebe43cd4df55e1f04d601d209a8af30f36cb
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-10-16 10:47:54 -0500

    Update bpmcreditoexpress.py

--- bpmcreditoexpress.py
+++ bpmcreditoexpress.py
@@ -1080,7 +1080,7 @@ def Indicadores_Proceso():
 # Configurar el menú de la aplicación
 menu = st.sidebar.selectbox(
     "Selecciona una página",
-    ["Consulta de solicitudes ", "Validación de Beneficiarios", "Enviar Oferta", "Gestión Comercial", "Gestión Ordenador del Gasto", "Indicadores Proceso"]
+    ["Captura de datos ", "Validación de Beneficiarios", "Enviar Oferta", "Gestión Comercial", "Gestión Ordenador del Gasto", "Indicadores Proceso"]
 )
 
 # Ejecutar la página seleccionada
