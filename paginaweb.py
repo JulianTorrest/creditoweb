69822ac92be9eb3c322f1f3036e7c9320f9e4b63
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-10-23 05:57:42 -0500

    Update paginaweb.py

--- paginaweb.py
+++ paginaweb.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # URL del archivo Excel en el repositorio público de GitHub
-url = 'https://github.com/usuario/repositorio/raw/main/ruta_al_archivo/archivo.xlsx'
+url = 'https://github.com/JulianTorrest/creditoweb/blob/main/tabla%20Condiciones%20Credito%20ICETEX%202024-1%20-%20Revisi%C3%B3n%20Pagina%20Web%20(1).xlsx'
 
 # Lee el archivo Excel desde la URL
 df = pd.read_excel(url)
