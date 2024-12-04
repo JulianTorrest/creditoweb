152e0ee8f3db7bba9921fb2f1e14bb694b63ad6d
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-12-04 06:11:29 -0500

    Update bpmcreditoexpress.py

--- bpmcreditoexpress.py
+++ bpmcreditoexpress.py
@@ -561,9 +561,9 @@ def enviar_oferta():
         st.success("Ofertas enviadas a todos los beneficiarios que pasaron las validaciones.")
 
     # Crear un DataFrame con los beneficiarios aprobados
-    st.session_state['df_aprobados'] = pd.DataFrame(beneficiarios_validados)
-    st.session_state['df_aprobados']['Año'] = año_seleccionado
-    st.session_state['df_aprobados']['Periodo'] = periodo_seleccionado
+    df_aprobados = pd.DataFrame(beneficiarios_validados)
+    df_aprobados['Año'] = año_seleccionado
+    df_aprobados['Periodo'] = periodo_seleccionado
 
     # Botón para descargar ofertas aprobadas
     if st.button("Descargar Excel con ofertas aprobadas"):
