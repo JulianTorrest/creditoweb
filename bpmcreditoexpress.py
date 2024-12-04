8cdfd7baedda284be6d483a3a801ec5fdc8423fc
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-12-03 23:05:44 -0500

    Update bpmcreditoexpress.py

--- bpmcreditoexpress.py
+++ bpmcreditoexpress.py
@@ -629,21 +629,21 @@ else:
 
 st.subheader("Cantidad de beneficiarios con errores por tipo de error")
 
-if len(beneficiarios_con_errores) > 0:
-    errores_count = {
-        "Score Crediticio": 0,
-        "Capacidad de Pago": 0,
-        "Estado Crédito": 0
-    }
+	if len(beneficiarios_con_errores) > 0:
+    		errores_count = {
+        	"Score Crediticio": 0,
+        	"Capacidad de Pago": 0,
+        	"Estado Crédito": 0
+    	}
 
-    for beneficiario in beneficiarios_con_errores:
-        errores = realizar_validaciones(beneficiario)
+    	for beneficiario in beneficiarios_con_errores:
+        	errores = realizar_validaciones(beneficiario)
         if "El score crediticio debe ser de mínimo 610 puntos." in errores:
-            errores_count["Score Crediticio"] += 1
+            	errores_count["Score Crediticio"] += 1
         if "Capacidad de pago insuficiente." in errores:
-            errores_count["Capacidad de Pago"] += 1
+            	errores_count["Capacidad de Pago"] += 1
         if "Estado de Crédito no aprobado." in errores:
-            errores_count["Estado Crédito"] += 1
+            	errores_count["Estado Crédito"] += 1
 
     fig2, ax2 = plt.subplots()
     ax2.bar(errores_count.keys(), errores_count.values(), color=['red', 'orange', 'green'])
