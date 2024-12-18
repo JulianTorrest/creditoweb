2d411de44f5787ac0ccc49ec4d4ab8ce6740ac14
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-12-17 21:41:07 -0500

    Update bpmcreditoexpress.py

--- bpmcreditoexpress.py
+++ bpmcreditoexpress.py
@@ -368,10 +368,6 @@ def captura_datos():
         df_beneficiarios = pd.DataFrame(beneficiarios_data)
 
         # Aplicar filtros
-        if estado_credito:  # Solo filtra si hay algo seleccionado
-    	    df_beneficiarios = df_beneficiarios[df_beneficiarios["Estado Crédito"].isin(estado_credito)]
-        if lista_sarlaft:  # Solo filtra si hay algo seleccionado
-            df_beneficiarios = df_beneficiarios[df_beneficiarios["Lista SARLAFT"].isin(lista_sarlaft)]
         if year:
             df_beneficiarios = df_beneficiarios[df_beneficiarios["Año"] == year]
         if periodo:
