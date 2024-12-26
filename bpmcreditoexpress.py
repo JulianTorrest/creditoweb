1a0ffb19230944308630bee9b49700a9a228e1f2
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-12-26 09:09:26 -0500

    Update bpmcreditoexpress.py

--- bpmcreditoexpress.py
+++ bpmcreditoexpress.py
@@ -694,12 +694,12 @@ def captura_datos():
 
 	# Enviar ofertas a los beneficiarios validados
         for beneficiario in beneficiarios_validados:
-	     oferta = beneficiario.copy()
-	     oferta["Interesado"] = random.choice(["Sí", "No", "Sí, pero después"])  # Asignar interés aleatorio
-	     oferta["GarantiaFirmada"] = random.choice([True, False])  # Asignar garantía aleatoria
-	     oferta["Valor"] = random.randint(3000000, beneficiario["Capacidad de Pago (COP)"])
-	     oferta["Año"] = año_seleccionado
-	     oferta["Periodo"] = periodo_seleccionado
+	 oferta = beneficiario.copy()
+	 oferta["Interesado"] = random.choice(["Sí", "No", "Sí, pero después"])  # Asignar interés aleatorio
+	 oferta["GarantiaFirmada"] = random.choice([True, False])  # Asignar garantía aleatoria
+	 oferta["Valor"] = random.randint(3000000, beneficiario["Capacidad de Pago (COP)"])
+	 oferta["Año"] = año_seleccionado
+	 oferta["Periodo"] = periodo_seleccionado
 	st.session_state['ofertas_en_proceso'].append(oferta)
 
     st.success("Ofertas enviadas a todos los beneficiarios que pasaron las validaciones.")
